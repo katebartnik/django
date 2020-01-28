9c4543dd3f597406f236084baa3c805feb2cbdfd
katebartnik <oferta.bartnik@gmail.com>
2020-01-28 20:36:46 +0100

    init django docker-compose

---
+++ src/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+
+# /hello
+def funkcja_widoku(request):
+    html = """<!doctype html>
+    <html>
+    <head></head>
+    <body>
+    Hello Alx!!
+    </body>
+    </html>"""
+
+    return HttpResponse(html)
+
+# /hello/Rafał
+def hello_name(request, name):
+    html = f"""<!doctype html>
+    <html>
+    <head></head>
+    <body>
+    Hello {name}!!
+    </body>
+    </html>"""
+    return HttpResponse(html)
+
+
+# np: /sum/1/2
+# /sum/<int:a>/<int:b>
+# /mul/1/2
+
+# /<op>/<int:a>/<int:b>
+def operacje(request, op, a, b):
+    if op == "sum":
+        wynik = a + b
+
+
+    return HttpResponse(wynik)
