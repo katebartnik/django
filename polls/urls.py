a861cec5834d97a0fdb380ccf6452843f63ff866
katebartnik <oferta.bartnik@gmail.com>
2020-03-26 20:26:47 +0100

    add polls

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from polls.views import funkcja_widoku, hello_name, operacje, index, questions_list
+from polls.views import funkcja_widoku, hello_name, operacje, index, questions_list, question_details
 
 urlpatterns = [
     path("", index, name="index"),
     path('hello/', funkcja_widoku),
     path('hello/<name>', hello_name),
     path('<op>/<int:a>/<int:b>', operacje),
-    path("questions/", questions_list, name="question-list")
+    path("pytanie/", questions_list, name="question-list"),
+    path("questions/<id>", question_details, name="question-details")
 ]
